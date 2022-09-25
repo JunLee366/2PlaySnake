37bb3f03cd48fbb834cb890e871df7839aa27e1a
David Wang <davidwang759@gmail.com>
2022-09-24 20:27:34 -0400

    lobby system implemented

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+let players = 0;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+      title: 'Express', 
+      id: players + 1
+  });
 });
+
+router.post('/', function(req, res, next) {
+  players += 1;
+  console.log(players);
+  // await for players
+  if (players <= 2) {
+    let id = req.body.id;
+    console.log(id);
+    res.render('play', { player_id: players });
+  }
+})
+
+router.post('/play', function(req, res, next) {
+  
+})
 
 module.exports = router;
